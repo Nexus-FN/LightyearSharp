ef67138f7399c7ebb6cd1935b52e992153ec5276
Finn <ernzerhofffinn@gmail.com>
2023-06-30 23:48:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+using Spectre.Console;
+
+namespace ChannelLauncher;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.Title = "Momentum Launcher";
+        
+        AnsiConsole.MarkupLine("Welcome, " + Environment.UserName + ", to the [underline blue]Momentum launcher.[/]");
+        AnsiConsole.MarkupLine("You can select an option using the arrow keys [underline blue]UP[/] and [underline blue]DOWN.[/]");
+        
+        string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\MomentumLauncher";
+
+        if (!Directory.Exists(appdata))
+        {
+            AnsiConsole.MarkupLine("[yellow]Appdata folder is missing, creating one for you...[/]");
+            Directory.CreateDirectory(appdata);
+        }
+
+            var option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("[blue]What do you want to do[/]?")
+                .PageSize(3)
+                .AddChoices(new[]
+                {
+                    "Start game", "Change fortnite path", "Exit"
+                }));
+
+        switch (option)
+        {
+            case "Start game":
+
+                if (!File.Exists(appdata + "\\path.txt"))
+                {
+                    Console.Clear();
+                    AnsiConsole.MarkupLine("[red]Fortnite path is missing, please set it first![/]");
+                    Main(args);
+                }
+
+                if (!File.Exists(appdata + "\\email.txt") || !File.Exists(appdata + "\\password.txt"))
+                {
+                    Console.Clear();
+                    AnsiConsole.MarkupLine("[red]Please Enter Your Email and Password! (Account Made From Discord Bot.)[/]");
+
+                    Console.Write("Email: ");
+                    string email = Console.ReadLine();
+                    File.WriteAllText(appdata + "\\email.txt", email);
+
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    File.WriteAllText(appdata + "\\password.txt", password);
+
+                    AnsiConsole.MarkupLine("[green]Email and Password Saved![/]");
+                }
+                
+                if (!File.Exists("redirect.json"))
+                {
+                    string fileContent = "{ \"name\": \"Buzz.dll\", \"download\": \"https://cdn.nexusfn.net/file/2023/06/TV.dll\" }";
+                    File.WriteAllText("redirect.json", fileContent);
+                }
+
+                string fileData = File.ReadAllText("redirect.json");
+
+                var jsonData = JsonConvert.DeserializeObject<Dictionary<string, object>>(fileData);
+
+                if (!jsonData.TryGetValue("name", out var dllNameObject) || !(dllNameObject is string dllName))
+                {
+                    throw new Exception("Invalid JSON structure: 'name' key is missing or not a string");
+                }
+
+                if (!jsonData.TryGetValue("download", out var dllDownloadObject) || !(dllDownloadObject is string dllDownload))
+                {
+                    throw new Exception("Invalid JSON structure: 'download' key is missing or not a string");
+                }
+
+                if (!dllName.EndsWith(".dll"))
+                {
+                    dllName += ".dll";
+                }
+
+                MomentumLauncher.Utilities.StartGame(File.ReadAllText(appdata + "\\path.txt"), dllDownload, dllName);
+                AnsiConsole.MarkupLine("Starting the client");
+                break;
+
+            case "Change fortnite path":
+                AnsiConsole.MarkupLine("Please enter the path to your fortnite folder");
+                string setPath = AnsiConsole.Ask<string>("Path: ");
+                string fortnitePath = Path.Combine(setPath, "FortniteGame", "Binaries", "Win64");
+
+                if (Directory.Exists(fortnitePath))
+                {
+                    Console.WriteLine(fortnitePath);
+                    File.WriteAllText(appdata + "\\path.txt", setPath);
+                    Console.Clear();
+                    AnsiConsole.MarkupLine("Path changed, you can now start the game");
+                    Main(args);
+                }
+                else
+                {
+                    Console.WriteLine(fortnitePath);
+                    AnsiConsole.MarkupLine("[red]Path is invalid![/]");
+                    Main(args);
+                }
+                break;
+            case "Exit":
+                Environment.Exit(0);
+                break;
+        }
+
+
+        Console.ReadKey(true);
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Spectre.Console;
+
+namespace MomentumLauncher;
+
+public class Utilities
+{
+
+    public static void DownloadFile(string Url, string Path) => new WebClient().DownloadFile(Url, Path);
+
+    public static async void StartGame(string gamePath, string dllDownload, string dllName)
+    {
+        AnsiConsole.MarkupLine("[blue]Starting game...[/]");
+
+        string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\MomentumLauncher";
+
+        if (!File.Exists(appdata + "\\FortniteClient-Win64-Shipping_BE.exe"))
+                DownloadFile("https://cdn.discordapp.com/attachments/958139296936783892/1000707724507623424/FortniteClient-Win64-Shipping_BE.exe", appdata + "\\FortniteClient-Win64-Shipping_BE.exe");
+        if (!File.Exists(appdata + "\\FortniteLauncher.exe"))
+                DownloadFile("https://cdn.discordapp.com/attachments/958139296936783892/1000707724818006046/FortniteLauncher.exe", appdata + "\\FortniteLauncher.exe");
+        if (!File.Exists(appdata + "\\" + dllName))
+                DownloadFile(dllDownload, appdata + "\\" + dllName);
+
+        string emailPath = Path.Combine(appdata, "email.txt");
+        string passwordPath = Path.Combine(appdata, "password.txt");
+        
+        string email = File.ReadAllText(emailPath);
+        string password = File.ReadAllText(passwordPath);
+        
+        Process launcher = new Process();
+        launcher.StartInfo.FileName = appdata + "\\FortniteLauncher.exe";
+        
+        Process shippingbe = new Process();
+        shippingbe.StartInfo.FileName = appdata + "\\FortniteClient-Win64-Shipping_BE.exe";
+        
+        Process shipping = new Process();
+        shipping.StartInfo.FileName = gamePath + "\\FortniteGame\\Binaries\\Win64\\FortniteClient-Win64-Shipping.exe";
+        shipping.StartInfo.Arguments =
+            $"-log -epicapp=Fortnite -epicenv=Prod -epiclocale=en-us -epicportal -skippatchcheck -nobe -fromfl=eac -fltoken=3db3ba5dcbd2e16703f3978d -nosplash -caldera=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiYmU5ZGE1YzJmYmVhNDQwN2IyZjQwZWJhYWQ4NTlhZDQiLCJnZW5lcmF0ZWQiOjE2Mzg3MTcyNzgsImNhbGRlcmFHdWlkIjoiMzgxMGI4NjMtMmE2NS00NDU3LTliNTgtNGRhYjNiNDgyYTg2IiwiYWNQcm92aWRlciI6IkVhc3lBbnRpQ2hlYXQiLCJub3RlcyI6IiIsImZhbGxiYWNrIjpmYWxzZX0.VAWQB67RTxhiWOxx7DBjnzDnXyyEnX7OljJm-j2d88G_WgwQ9wrE6lwMEHZHjBd1ISJdUO1UVUqkfLdU5nofBQ -AUTH_LOGIN={email} -AUTH_PASSWORD={password} -AUTH_TYPE=epic";
+        shipping.StartInfo.UseShellExecute = false;
+        shipping.StartInfo.RedirectStandardOutput = true;
+        shipping.StartInfo.RedirectStandardError = true;
+        
+        launcher.Start();
+        shippingbe.Start();
+        shipping.Start();
+        
+        MomentumLauncher.Injector.Inject(shipping.Id, appdata + "\\" + dllName);
+        Environment.Exit(0);
+
+
+    }
+    
+}
